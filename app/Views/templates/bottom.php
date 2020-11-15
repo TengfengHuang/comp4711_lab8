aa3d28f70918dfc22d02bdcabbc9f1b5efbc193f
TengfengHuang <760201280@qq.com>
2020-11-15 23:13:42 +0800

    Add top of webpage.

--- app/Views/templates/bottom.php
+++ app/Views/templates/bottom.php
@@ -1,13 +1,3 @@
-<?php
-
-
 {footer}
 </body>
 </html>
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
